6c7f12942f8a7f4767b440011b53dd5eba05f942
ashpatx <thingslearn3@gmail.com>
2023-08-26 17:14:30 -0400

    Create main.js

---
+++ icodethis/Check In (Tailwind)/main.js
@@ -0,0 +1,3 @@
+//Lottie Animation Script
+const lottieContainer = document.getElementById("lottieAnimation");
+lottieContainer.innerHTML = `<dotlottie-player src="https://lottie.host/29fcef8a-14ce-4c99-8a98-e471d25face4/xyyy4lYBZm.lottie" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>`;
