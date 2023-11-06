43ef8dc152498eb4076724fd94df36c0598e7ea0
ashpatx <thingslearn3@gmail.com>
2023-11-06 15:21:56 -0500

    Create countdown.js

---
+++ icodethis/Countdown (Tailwind)/countdown.js
@@ -0,0 +1,26 @@
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+
+function updateCountdownTime() {
+  const currentTime = new Date();
+  const currentYear = currentTime.getFullYear();
+  const thanksgivingTime = new Date(
+    `November 24, ${currentYear} 2023 00:00:00`
+  );
+  const diff = thanksgivingTime - currentTime;
+
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const hours = Math.floor((diff / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((diff / 1000 / 60) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  daysElement.textContent = days;
+  hoursElement.textContent = hours < 10 ? "0" + hours : hours;
+  minutesElement.textContent = minutes < 10 ? "0" + minutes : minutes;
+  secondsElement.textContent = seconds < 10 ? "0" + seconds : seconds;
+}
+
+setInterval(updateCountdownTime, 1000);
+updateCountdownTime();
