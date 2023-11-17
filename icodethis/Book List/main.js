690da9347e71b3d2cdf1973c127b441b86bbdba8
ashpatx <thingslearn3@gmail.com>
2023-11-17 11:31:07 -0500

    Create main.js

---
+++ icodethis/Book List/main.js
@@ -0,0 +1,39 @@
+//books array
+const books = [
+  {
+    title: "Yellowface",
+    category: "Fiction",
+    author: "R. F. Kuang",
+    coverImg: ["https://images.booksense.com/images/833/250/9780063250833.jpg",
+  },
+  {
+    title: "Loot",
+    category: "Fiction",
+    author: "Tania James",
+    coverImg: ["https://images1.penguinrandomhouse.com/cover/9780593535974",
+  },
+               {
+    title: "Make it Stick",
+    category: "Nonfiction",
+    author: "Peter B, Henry R, Mark M",
+    coverImg: ["https://images.squarespace-cdn.com/content/v1/564a53ace4b0ef1eb2daff41/1524810192099-QZZKCRPMM2D2692XHZZG/Cover+Make+it+Stick.jpg?format=1500w",
+  },
+                 {
+    title: "Being Mortal",
+    category: "Nonfiction",
+    author: "Atul Gawande",
+    coverImg: ["https://upload.wikimedia.org/wikipedia/en/thumb/1/1d/Being_Mortal.jpg/220px-Being_Mortal.jpg",
+  },
+                {
+    title: "Structure and Interpretation of Computer Programs",
+    category: "Nonfiction",
+    author: "Harold A, Gerald S, Julie S",
+    coverImg: ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/SICP_cover.jpg/220px-SICP_cover.jpg",
+  },
+                {
+    title: "Mindstorms",
+    category: "Nonfiction",
+    author: "Seymour Papert",
+    coverImg: ["https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Mindstorms_%28book%29.jpg/220px-Mindstorms_%28book%29.jpg",
+  }
+];
