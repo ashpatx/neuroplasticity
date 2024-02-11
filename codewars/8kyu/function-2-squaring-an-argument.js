4e50f398674b0b9d3fbae508d88cb1983a38ce80
ashpatx <thingslearn3@gmail.com>
2024-02-11 12:49:48 -0500

    Create function-2-squaring-an-argument.js

---
+++ codewars/8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,9 @@
+//Description: Now you have to write a function that takes an argument and returns the square of it.
+
+//Solution: 
+function square(arg) {
+  return arg ** 2
+}
+
+//Refactored Improved Solution: arrow function
+const square = x => x ** 2;
