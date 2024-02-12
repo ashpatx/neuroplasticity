5dbfd5eb84e7c21c1e090b3c3d92ffd017782f1f
ashpatx <thingslearn3@gmail.com>
2024-02-11 20:58:52 -0500

    Create script.js

---
+++ creative coding/Sticky Notes App/script.js
@@ -0,0 +1,22 @@
+const container02 = document.getElementsByClassName('container02')[0];
+const container03 = document.getElementsByClassName('container03')[0];
+const approveBtn = document.getElementById("approve-button"); 
+const deleteBtn = document.getElementById("delete-button");
+let i = 0;
+
+
+approveBtn.addEventListener("click", function(){
+  addNote();
+})
+
+deleteBtn.addEventListener("click", function(){
+  typeNote();
+})
+
+function typeNote(){
+
+}
+
+function addNote(){
+  
+}
